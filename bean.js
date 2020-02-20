dc10f53cd5a8dbea038a724a674b64fc2b9454d0
PaulDepew <Paul@lostamongpines.com>
2020-02-20 12:29:46 -0800

    consolidated js files and created function callers

--- bean.js
+++ bean.js
@@ -1,10 +1,48 @@
 
-var person  = prompt('What Coffee do you like') ;
+function bean(){
 
-if  (person == "") {
-    txt= "Goodbye!" ;
-} else {
-    txt = "Hmm... Interesting Choice..."
+    var person  = prompt('What Coffee do you like') ;
+
+    if  (person == "") {
+        txt= "Goodbye!" ;
+    } else {
+        txt = "Hmm... Interesting Choice..."
+    }
+    
+    return txt 
+
+   
 }
 
-document.write("<h4>" + txt + "</h4>")
+function coffee(){
+    var txt;
+
+var r = confirm("Do you love coffee!");
+if (r == true) {
+  txt = "ONE OF US!";
+} else {
+  txt = "Maybe this isn't the place for you...";
+}
+
+  return txt
+
+}
+
+function time(){
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = "Not Coffee Time!"
+} else if (hourNow > 12) {
+    greeting = "Coffee? Sure!" ;
+} else if (hourNow >0) {
+    greeting = "MORE COFFEE NOW!" ;
+} else {
+    greeting = "Always Coffee Time" ;
+}
+
+return greeting
+}
